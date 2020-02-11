2066bfcc511c39455d338e959cf3d1d9ad9c00b7
Mark G <mark.garcia.8001@gmail.com>
2020-02-11 09:32:57 -0800

    2.11

--- src/Candy.java
+++ src/Candy.java
@@ -1,2 +1,15 @@
 public abstract class Candy extends DessertItem {
+    private int calories;
+    private int amount;
+    private final double CPP = .89; //cost per pound
+
+    public Candy(int a, int c){
+        this.amount = a;
+        this.calories = c;
+    }
+
+    @Override
+    public double getCost() {
+        return amount * CPP;
+    }
 }
--- src/Cookie.java
+++ src/Cookie.java
@@ -1,2 +1,21 @@
 public abstract class Cookie extends DessertItem {
+    private int calories;
+    private int amount;
+    private final double CPD = 3.99/12; //cost per dozen
+
+    public Cookie(int a, int c) {
+        super.name = "Cookie";
+        this.amount = a;
+        this.calories = c;
+    }
+
+    @Override
+    public double getCost() {
+        return amount * CPD;
+    }
+
+    public int getCalories() {
+        return amount * calories;
+    }
+
 }
